75287e9f4fa9374f3ad63862cc3655c41d4cf1a8
templar-ajay <templar.ajay@gmail.com>
2023-12-04 22:49:52 +0530

    initial commit

---
+++ src/app/[uid]/page.tsx
@@ -0,0 +1,53 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { SliceZone } from "@prismicio/react";
+
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+import Footer from "@/components/Footer";
+
+type Params = { uid: string };
+
+export default async function Page({ params }: { params: Params }) {
+  const client = createClient();
+  const page = await client
+    .getByUID("landing_page", params.uid)
+    .catch(() => notFound());
+
+  return (
+    <>
+      <SliceZone slices={page.data.slices} components={components} />
+      <Footer></Footer>
+    </>
+  );
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const client = createClient();
+  const page = await client
+    .getByUID("landing_page", params.uid)
+    .catch(() => notFound());
+
+  const { meta_title, meta_description, meta_image } = page.data;
+
+  return {
+    title: meta_title,
+    description: meta_description,
+    openGraph: {
+      images: [meta_image?.url || "./fallback_image_path"],
+    },
+  };
+}
+
+export async function generateStaticParams() {
+  const client = createClient();
+  const pages = await client.getAllByType("landing_page");
+
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
+}
